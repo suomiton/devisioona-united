e5b9cc41d238108d7fa3b0ad2fee28b36818fe4c
Matti Petrelius <matti.petrelius@gmail.com>
2019-04-04 09:35:06 +0300

    Add crawling data from palloliitto scripts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { WidthWrapper } from "../components/common"
